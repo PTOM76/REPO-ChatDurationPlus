27b371a9f9cf88aa758fad8d9c050f6cd04db6d5
PTOM76 <ptms76@outlook.jp>
2025-08-13 21:57:04 +0900

    initial commit

---
+++ TimerData.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using BepInEx;
+
+public class TimerData
+{
+    public float extraTime;
+
+    public static float defaultExtraTime = 10f;
+    public static string path = Path.Combine(Paths.ConfigPath, "ChatDurationPlus.json");
+
+    static TimerData()
+    {
+        
+        if (!File.Exists(path))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+            File.WriteAllText(path, JsonConvert.SerializeObject(new { extraTime = defaultExtraTime }, Formatting.Indented));
+        }
+        else
+        {
+            try
+            {
+                string json = File.ReadAllText(path);
+                var data = JsonConvert.DeserializeObject<TimerData>(json);
+                if (data != null)
+                    defaultExtraTime = data.extraTime;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    public TimerData()
+    {
+        extraTime = defaultExtraTime;
+    }
+}
---
+++ WorldSpaceUITTSPatch.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace ChatDurationPlus;
+
+[HarmonyPatch(typeof(WorldSpaceUITTS))]
+static class WorldSpaceUITTSPatch
+{
+    private static readonly ConditionalWeakTable<WorldSpaceUITTS, TimerData> timers = new ConditionalWeakTable<WorldSpaceUITTS, TimerData>();
+
+    [HarmonyPrefix, HarmonyPatch(nameof(WorldSpaceUITTS.Update))]
+    private static bool DisplayTimePatch(WorldSpaceUITTS __instance)
+    {
+        TimerData timerData = timers.GetOrCreateValue(__instance);
+
+        if (__instance.ttsVoice != null && __instance.ttsVoice.isSpeaking) 
+            return true;
+
+        if (timerData.extraTime > 0f)
+        {
+            timerData.extraTime -= Time.deltaTime;
+            __instance.UpdatePositionAndAlpha();
+            return false;
+        }
+
+        return true;
+    }
+}
+
+public static class WorldSpaceUITTS_Extension
+{
+    public static void UpdatePositionAndAlpha(this WorldSpaceUITTS __instance)
+    {
+        var textAlphaField = typeof(WorldSpaceUITTS).GetField("textAlpha", BindingFlags.NonPublic | BindingFlags.Instance);
+        var textColorField = typeof(WorldSpaceUITTS).GetField("textColor", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        if (textAlphaField == null || textColorField == null) return;
+
+        float textAlpha = (float) textAlphaField.GetValue(__instance);
+        textAlpha = Mathf.Lerp(textAlpha, 1f, 30f * Time.deltaTime);
+        textAlphaField.SetValue(__instance, textAlpha);
+
+        Color textColor = (Color) textColorField.GetValue(__instance);
+        __instance.text.color = new Color(textColor.r, textColor.g, textColor.b, textAlpha);
+
+        if (__instance.followTransform)
+        {
+            __instance.followPosition = Vector3.Lerp(__instance.followPosition, __instance.followTransform.position, 10f * Time.deltaTime);
+        }
+        __instance.worldPosition = __instance.followPosition;
+    }
+}
